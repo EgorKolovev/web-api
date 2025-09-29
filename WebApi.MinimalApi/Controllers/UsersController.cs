edf75978fa5665ff5a150fa7afcf91220d667740
EgorKolovev <118982453+EgorKolovev@users.noreply.github.com>
2025-09-29 15:33:09 +0500

    не ну надеюсь работает

--- WebApi.MinimalApi/Controllers/UsersController.cs
+++ WebApi.MinimalApi/Controllers/UsersController.cs
@@ -1,4 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using WebApi.MinimalApi.Domain;
 using WebApi.MinimalApi.Models;
 
@@ -8,20 +16,208 @@ namespace WebApi.MinimalApi.Controllers;
 [ApiController]
 public class UsersController : Controller
 {
-    // Чтобы ASP.NET положил что-то в userRepository требуется конфигурация
-    public UsersController(IUserRepository userRepository)
+    private const int DefaultPageNumber = 1;
+    private const int DefaultPageSize = 10;
+    private const int MaxPageSize = 20;
+
+    private static readonly JsonSerializerSettings PaginationSerializerSettings = new()
     {
+        ContractResolver = new CamelCasePropertyNamesContractResolver()
+    };
+
+    private readonly IUserRepository userRepository;
+    private readonly IMapper mapper;
+    private readonly LinkGenerator linkGenerator;
+
+    public UsersController(IUserRepository userRepository, IMapper mapper, LinkGenerator linkGenerator)
+    {
+        this.userRepository = userRepository ?? throw new ArgumentNullException(nameof(userRepository));
+        this.mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        this.linkGenerator = linkGenerator ?? throw new ArgumentNullException(nameof(linkGenerator));
     }
 
-    [HttpGet("{userId}")]
-    public ActionResult<UserDto> GetUserById([FromRoute] Guid userId)
+    [HttpGet("{userId}", Name = nameof(GetUserById))]
+    [Produces("application/json", "application/xml")]
+    public ActionResult<UserDto> GetUserById([FromRoute] string userId)
     {
-        throw new NotImplementedException();
+        var userResult = TryGetExistingUser(userId, NotFound, out var userEntity, out var errorResult);
+        if (!userResult)
+            return errorResult!;
+
+        var userDto = mapper.Map<UserDto>(userEntity);
+        return Ok(userDto);
+    }
+
+    [HttpHead("{userId}")]
+    [Produces("application/json", "application/xml")]
+    public IActionResult HeadUserById([FromRoute] string userId)
+    {
+        var userResult = TryGetExistingUser(userId, NotFound, out var userEntity, out var errorResult);
+        if (!userResult)
+            return errorResult!;
+
+        var userDto = mapper.Map<UserDto>(userEntity);
+        return Ok(userDto);
+    }
+
+    [HttpGet(Name = nameof(GetUsers))]
+    [Produces("application/json", "application/xml")]
+    public ActionResult<IEnumerable<UserDto>> GetUsers([FromQuery] int? pageNumber, [FromQuery] int? pageSize)
+    {
+        var actualPageNumber = pageNumber.HasValue ? Math.Max(pageNumber.Value, DefaultPageNumber) : DefaultPageNumber;
+        var actualPageSize = pageSize.HasValue ? Math.Clamp(pageSize.Value, 1, MaxPageSize) : DefaultPageSize;
+
+        var pageList = userRepository.GetPage(actualPageNumber, actualPageSize);
+        var users = mapper.Map<IEnumerable<UserDto>>(pageList);
+
+        AddPaginationHeader(pageList, actualPageNumber, actualPageSize);
+
+        return Ok(users);
     }
 
     [HttpPost]
-    public IActionResult CreateUser([FromBody] object user)
+    [Produces("application/json", "application/xml")]
+    public IActionResult CreateUser([FromBody] CreateUserDto? user)
     {
-        throw new NotImplementedException();
+        if (user is null)
+            return BadRequest();
+
+        ValidateLoginCharacters(user.Login);
+
+        if (!ModelState.IsValid)
+            return UnprocessableEntity(ModelState);
+
+        var userEntity = mapper.Map<UserEntity>(user);
+        var createdUser = userRepository.Insert(userEntity);
+
+        return CreatedAtRoute(
+            nameof(GetUserById),
+            new { userId = createdUser.Id },
+            createdUser.Id);
+    }
+
+    [HttpPut("{userId}")]
+    [Produces("application/json", "application/xml")]
+    public IActionResult UpdateUser([FromRoute] string userId, [FromBody] UpdateUserDto? user)
+    {
+        if (!Guid.TryParse(userId, out var id))
+            return BadRequest();
+
+        if (user is null)
+            return BadRequest();
+
+        if (!ModelState.IsValid)
+            return UnprocessableEntity(ModelState);
+
+        var entityToUpdate = mapper.Map(user, new UserEntity(id));
+        userRepository.UpdateOrInsert(entityToUpdate, out var isInserted);
+
+        if (isInserted)
+        {
+            return CreatedAtRoute(
+                nameof(GetUserById),
+                new { userId = entityToUpdate.Id },
+                entityToUpdate.Id);
+        }
+
+        return NoContent();
+    }
+
+    [HttpPatch("{userId}")]
+    [Produces("application/json", "application/xml")]
+    public IActionResult PartiallyUpdateUser([FromRoute] string userId, [FromBody] JsonPatchDocument<UpdateUserDto>? patchDoc)
+    {
+        var userResult = TryGetExistingUser(userId, NotFound, out var existingUser, out var errorResult);
+        if (!userResult)
+            return errorResult!;
+
+        if (patchDoc is null)
+            return BadRequest();
+
+        var userToPatch = mapper.Map<UpdateUserDto>(existingUser);
+        patchDoc.ApplyTo(userToPatch, ModelState);
+
+        if (!ModelState.IsValid)
+            return UnprocessableEntity(ModelState);
+
+        if (!TryValidateModel(userToPatch))
+            return UnprocessableEntity(ModelState);
+
+        if (!ModelState.IsValid)
+            return UnprocessableEntity(ModelState);
+
+        mapper.Map(userToPatch, existingUser);
+        userRepository.Update(existingUser);
+
+        return NoContent();
+    }
+
+    [HttpDelete("{userId}")]
+    public IActionResult DeleteUser([FromRoute] string userId)
+    {
+        var userResult = TryGetExistingUser(userId, NotFound, out var userEntity, out var errorResult);
+        if (!userResult)
+            return errorResult!;
+
+        userRepository.Delete(userEntity.Id);
+        return NoContent();
+    }
+
+    [HttpOptions]
+    public IActionResult GetUsersOptions()
+    {
+        Response.Headers["Allow"] = new[] { "GET", "POST", "OPTIONS" };
+        return Ok();
+    }
+
+    private void ValidateLoginCharacters(string? login)
+    {
+        if (string.IsNullOrEmpty(login))
+            return;
+
+        if (login.Any(ch => !char.IsLetterOrDigit(ch)))
+            ModelState.AddModelError("Login", "Login should contain only letters or digits");
+    }
+
+    private bool TryGetExistingUser(string userId, Func<IActionResult> invalidIdResultFactory, out UserEntity? userEntity, out IActionResult? errorResult)
+    {
+        userEntity = null;
+        errorResult = null;
+
+        if (!Guid.TryParse(userId, out var id))
+        {
+            errorResult = invalidIdResultFactory();
+            return false;
+        }
+
+        var entity = userRepository.FindById(id);
+        if (entity is null)
+        {
+            errorResult = NotFound();
+            return false;
+        }
+
+        userEntity = entity;
+        return true;
+    }
+
+    private void AddPaginationHeader(PageList<UserEntity> pageList, int pageNumber, int pageSize)
+    {
+        var paginationHeader = new
+        {
+            previousPageLink = pageList.HasPrevious ? BuildUsersPageLink(pageNumber - 1, pageSize) : null,
+            nextPageLink = pageList.HasNext ? BuildUsersPageLink(pageNumber + 1, pageSize) : null,
+            totalCount = pageList.TotalCount,
+            pageSize = pageList.PageSize,
+            currentPage = pageList.CurrentPage,
+            totalPages = pageList.TotalPages
+        };
+
+        Response.Headers["X-Pagination"] = JsonConvert.SerializeObject(paginationHeader, PaginationSerializerSettings);
+    }
+
+    private string? BuildUsersPageLink(int pageNumber, int pageSize)
+    {
+        return linkGenerator.GetUriByRouteValues(HttpContext, nameof(GetUsers), new { pageNumber, pageSize });
     }
 }
--- WebApi.MinimalApi/Program.cs
+++ WebApi.MinimalApi/Program.cs
@@ -1,10 +1,45 @@
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using WebApi.MinimalApi.Domain;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using WebApi.MinimalApi.Domain;
+using WebApi.MinimalApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.UseUrls("http://localhost:5000");
-builder.Services.AddControllers()
-    .ConfigureApiBehaviorOptions(options => {
+
+builder.Services.AddSingleton<IUserRepository, InMemoryUserRepository>();
+
+builder.Services.AddControllers(options =>
+    { 
+        options.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+        options.ReturnHttpNotAcceptable = true;
+        options.RespectBrowserAcceptHeader = true;
+    })
+    .ConfigureApiBehaviorOptions(options =>
+    {
         options.SuppressModelStateInvalidFilter = true;
         options.SuppressMapClientErrors = true;
+    })
+    .AddNewtonsoftJson(options =>
+    {
+        options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+        options.SerializerSettings.DefaultValueHandling = DefaultValueHandling.Populate;
     });
+
+builder.Services.AddAutoMapper(cfg =>
+{
+    cfg.CreateMap<UserEntity, UserDto>()
+        .ForMember(dest => dest.FullName,
+            opt => opt.MapFrom(src => string.Join(" ", src.LastName, src.FirstName).Trim()));
+    cfg.CreateMap<CreateUserDto, UserEntity>();
+    cfg.CreateMap<UpdateUserDto, UserEntity>();
+    cfg.CreateMap<UserEntity, UpdateUserDto>();
+});
 
 var app = builder.Build();
 
